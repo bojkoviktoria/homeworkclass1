d3f27678c3f000479fd88650dafd51005e072fa1
BojkoViktoria <bojkovoktoria404@gmail.com>
2018-09-04 14:20:30 +0300

    Initial commit

---
+++ homework2.php
@@ -0,0 +1,35 @@
+<?php
+
+class Worker {
+    private $name;
+    private $age;
+    private $salary;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setName($name)
+    {
+       $this->name = $name;
+    }
+    public function getAge()
+    {
+        return $this->age;
+    }
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+    public function  setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+    worker1
+}
+
+
